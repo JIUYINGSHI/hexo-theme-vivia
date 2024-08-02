41cdc8a860e2ff056b5aa1ab07563d1018864051
JIUYING <jiuyingshi914@gmail.com>
2024-08-02 08:48:59 +0800

    Update article-helper.js

--- scripts/article-helper.js
+++ scripts/article-helper.js
@@ -2,3 +2,7 @@ hexo.extend.helper.register('symbolsCount', function(post) {
   if (!post.content) return 0;
   return post.content.length;
 });
+
+hexo.extend.helper.register('symbolsCount', function (post) {
+  return post.content.length;
+});
